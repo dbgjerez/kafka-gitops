b021ade1d82d33eb9596ce658a295342c67c1521
David Borrego <dborrego@redhat.com>
2022-01-23 09:33:21 +0100

    start

---
+++ minikube/minikube.sh
@@ -0,0 +1,17 @@
+minikube start --cpus=4 --memory='8g'
+minikube addons enable ingress
+
+kubectl --namespace ingress-nginx wait \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=120s
+
+INGRESS_HOST=$(minikube ip)
+HOST=argocd.$INGRESS_HOST.nip.io
+NS=argocd
+
+## user=admin
+## password
+# kubectl -n argocd get secret argocd-cluster -o json | jq -r '.data["admin.password"]' | base64 -d
+
+printf "URL: $HOST\n"
